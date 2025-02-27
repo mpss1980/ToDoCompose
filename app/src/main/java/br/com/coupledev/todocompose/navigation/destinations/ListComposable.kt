9c0762c6c593a760ca7a467272cf0a02e3b9c71d
Marcos Sousa <marcosp.sousa@gmail.com>
2025-02-27 08:11:37 -0300

    Finished list screen fab

--- app/src/main/java/br/com/coupledev/todocompose/navigation/destinations/ListComposable.kt
+++ app/src/main/java/br/com/coupledev/todocompose/navigation/destinations/ListComposable.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import br.com.coupledev.todocompose.ui.screens.list.ListScreen
 import br.com.coupledev.todocompose.util.Constants.LIST_ARGUMENT_KEY
 import br.com.coupledev.todocompose.util.Constants.LIST_SCREEN
 
@@ -16,6 +17,6 @@ fun NavGraphBuilder.listComposable(
             type = NavType.StringType
         })
     ) {
-
+        ListScreen(navigateToTaskScreen = navigateToTaskScreen)
     }
 }
